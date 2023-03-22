dd25025fe89cbb9918936a9f4bd42c08aa160afb
unknown <zidanmaulana595@gmail.com>
2023-03-22 14:02:34 +0700

    commit

--- Website PHP/main.php
+++ Website PHP/main.php
@@ -22,14 +22,6 @@
             <h3>Hello I'ts Me</h3>
             <h1>Muhammad Zidan Maulana</h1>
             <h3>And I'm a <span>University Student</span></h3>
-            <?php $hari="08"; ?>
-            <?php $bulan="Mei"; ?>
-            <?php $tahun="2003"; ?>
-            <?php $umur="19"; ?>
-            <p><?php echo"Saya adalah seorang mahasiswa UPN Veteran Jawa Timur yang lahir dikota Surabaya pada tanggal $hari $bulan $tahun
-            . Sekarang saya sedang melaksanakan studi S1 di jurusan Informatika dan sekarang saya sedang menempuh awal semester 4.
-             Dan sekarang saya juga sudah berumur $umur tahun. Tujuan saya sekarang adalah mengumpulkan pengalaman sebanyak-banyaknya
-             dan bisa lulus dengan nilai yang memuaskan dan tepat waktu."?></p>
             <div class="sosial-media">
                 <a href="https://web.facebook.com/profile.php?id=100022908092370" style="--i:7"><i class='bx bxl-facebook'></i></a>
                 <a href="https://www.instagram.com/zidanmaulana___/?hl=id" style="--i:8"><i class='bx bxl-instagram-alt'></i></a>
